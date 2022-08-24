bee56eabfc8e07accf738687221dd9749b3debbd
ChenZhaoYu <790348264@qq.com>
2022-08-24 09:50:17 +0800

    feat: layout

---
+++ src/layout/default-layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-layout>
+    <a-layout>
+      <a-layout>
+        <a-layout-sider></a-layout-sider>
+        <a-layout>
+          <a-layout-content>
+            <router-view></router-view>
+          </a-layout-content>
+        </a-layout>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
---
+++ src/layout/page-layout.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view v-slot="{ Component, route }">
+    <transition name="fade" mode="out-in" appear>
+      <component :is="Component" :key="route.fullPath" />
+    </transition>
+  </router-view>
+</template>
+
+<script setup lang="ts"></script>
