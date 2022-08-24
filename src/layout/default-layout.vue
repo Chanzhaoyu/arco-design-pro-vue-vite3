25d2c9909dbbf48eab1b7c332fc73e5ed8cda12e
ChenZhaoYu <790348264@qq.com>
2022-08-24 14:14:43 +0800

    feat: layout page

--- src/layout/default-layout.vue
+++ src/layout/default-layout.vue
@@ -1,8 +1,15 @@
 <template>
-  <a-layout>
+  <a-layout class="layout" :class="{ mobile: appStore.hideMenu }">
+    <div v-if="navbar" class="layout-navbar"> layout-navbar </div>
     <a-layout>
       <a-layout>
-        <a-layout-sider></a-layout-sider>
+        <a-layout-sider
+          class="layout-sider"
+          breakpoint="xl"
+          :collapsible="true"
+        >
+          <div class="menu-wrapper"> menu</div>
+        </a-layout-sider>
         <a-layout>
           <a-layout-content>
             <router-view></router-view>
@@ -13,6 +20,22 @@
   </a-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref, computed, watch, provide } from 'vue';
+  import { useRouter, useRoute } from 'vue-router';
+  import { useAppStore, useUserStore } from '@/store';
+  import usePermission from '@/hooks/permission';
+
+  const appStore = useAppStore();
+  const userStore = useUserStore();
+  const router = useRouter();
+  const route = useRoute();
+  const permission = usePermission();
+  const navbarHeight = `60px`;
+  const navbar = computed(() => appStore.navbar);
+  const renderMenu = computed(() => appStore.menu);
+  const hideMenu = computed(() => appStore.hideMenu);
+  const footer = computed(() => appStore.footer);
+</script>
 
 <style></style>
--- src/layout/page-layout.vue
+++ src/layout/page-layout.vue
@@ -1,9 +1,18 @@
 <template>
   <router-view v-slot="{ Component, route }">
     <transition name="fade" mode="out-in" appear>
-      <component :is="Component" :key="route.fullPath" />
+      <component
+        :is="Component"
+        v-if="route.meta.ignoreCache"
+        :key="route.fullPath"
+      />
+      <keep-alive v-else>
+        <component :is="Component" :key="route.fullPath" />
+      </keep-alive>
     </transition>
   </router-view>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup></script>
+
+<style scoped lang="less"></style>
