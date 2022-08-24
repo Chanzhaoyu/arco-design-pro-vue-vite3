855b5970192c269234848e8a0f0cd2e9002468bc
ChenZhaoYu <790348264@qq.com>
2022-08-24 09:18:26 +0800

    chore: 配置文件

---
+++ config/vite.config.dev.ts
@@ -0,0 +1,25 @@
+import { mergeConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import baseConfig from './vite.config.base';
+
+export default mergeConfig(
+  {
+    mode: 'development',
+    server: {
+      open: false,
+      port: 1002,
+      host: '0.0.0.0',
+      fs: {
+        strict: true,
+      },
+    },
+    plugins: [
+      eslint({
+        cache: false,
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+        exclude: ['node_modules'],
+      }),
+    ],
+  },
+  baseConfig
+);
